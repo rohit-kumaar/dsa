4a40e429cc1fd547e2cf3bc1fca26d463a07d91e
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-06-13 22:58:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function script(url) {
+  const head = document.getElementsByTagName("head")[0];
+  const script = document.createElement("script");
+  script.src = url;
+  script.type = "module";
+  head.appendChild(script);
+}
+
